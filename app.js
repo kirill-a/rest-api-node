e15b7cd227ad2309a512581c73d7183c46e5bda8
Kirill Ageev <kirill_ageev@epam.com>
2017-12-12 17:52:16 +0300

    Add users methods with db

--- app.js
+++ app.js
@@ -1,11 +1,65 @@
-var express = require('express')
+var express = require('express'),
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser')
 
-var app = express();
+var db = mongoose.connect('mongodb://127.0.0.1:27017/userAPI',
+{
+    useMongoClient: true,
+    bufferMaxEntries: 0},
+    function(err) {
+        if (err) {
+            console.err(err);
+        } else {
+            console.log('Connected to db');
+        }
+    }
+)
 
+var User = require('./models/userModel')
+var app = express()
 var port = process.env.PORT || 3000
 
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json)
+
+var router = express.Router()
+
+router.route('/users')
+    .post((req, res) => {
+        var user = new User(req.body)
+
+        console.log(user)
+        res.send(user)
+    })
+    .get((req, res) => {
+        var query = {}
+        if (req.query.jobTitle) {
+            query.jobTitle = req.query.jobTitle
+        }
+        User.find(query, (err, usersResult) => {
+            if (err) {
+                res.status(500).send(err)
+            } else {
+                res.json(usersResult)
+            }
+        })
+    })
+
+router.route('/users/:userId')
+    .get((req, res) => {
+        User.findById(req.params.userId, (err, usersResult) => {
+            if (err) {
+                res.status(500).send(err)
+            } else {
+                res.json(usersResult)
+            }
+        })
+    })
+
+app.use('/api', router)
+
 app.get('/', (req, res) => {
-    res.send("Welcome")
+    res.send("Welcome to users API")
 })
 
 app.listen(port, () => {
---
+++ models/userModel.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema
+
+var userModel = new Schema({
+    firstName: { type: String },
+    lastName:  { type: String },
+    jobTitle:  { type: String },
+    isFulltime:  { type: Boolean, dafault: true },
+}, { bufferCommands: false })
+
+module.exports = mongoose.model('User', userModel)
