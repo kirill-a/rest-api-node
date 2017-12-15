4b09e61764d1e68a85878d11a5857e305caa9667
Kirill Ageev <kirill_ageev@epam.com>
2017-12-15 14:27:47 +0300

    Add tests

--- tests/integration-test.js
+++ tests/integration-test.js
@@ -3,8 +3,35 @@
 var request = require('request')
 var expect = require('chai').expect
 var proxyURL = 'http://127.0.0.1:8000/api'
+var userId
 
 describe('Given we have REST API with users info', function () {
+  describe('When POST new user', function () {
+    it('it should return all users', function (done) {
+      var options = {
+        method: 'POST',
+        url: proxyURL + '/users',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        form: {
+          'firstName': 'Mike 2',
+          'lastName': 'Doug',
+          'jobTitle': 'Tester',
+          'isFulltime': false
+        }
+      }
+      request(options, function (error, response, body) {
+        if (error) {
+          throw Error('Error calling API' + error)
+        }
+        expect(response.statusCode).to.equal(201)
+        userId = JSON.parse(body)._id
+        console.log(userId)
+        done()
+      })
+    })
+  })
   describe('When GET users list', function () {
     it('it should return all users', function (done) {
       var options = {
@@ -18,6 +45,22 @@ describe('Given we have REST API with users info', function () {
           throw Error('Error calling API' + error)
         }
         expect(response.statusCode).to.equal(200)
+        done()
+      })
+    })
+  })
+  describe('When DELETE new user', function () {
+    it('it should return all users', function (done) {
+      var options = {
+        method: 'DELETE',
+        url: proxyURL + '/users/' + userId
+      }
+      request(options, function (error, response, body) {
+        if (error) {
+          throw Error('Error calling API' + error)
+        }
+        expect(response.statusCode).to.equal(204)
+        userId = body._id
         done()
       })
     })
