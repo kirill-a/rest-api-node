2cef546d4bc8b2df406e502c28a111c2059f85d7
Kirill Ageev <kirill_ageev@epam.com>
2017-12-15 17:17:40 +0300

    Add tests for CRUD operations

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Simplest REST API on Node.js and Express, tests with Mocha and Chai.
 `npm run-script lint` -- execute eslint on src/ dir
 
 `npm test` -- run mocha tests (server must be running)
+
+*Note that you need `mongodb` installed*
--- tests/integration-test.js
+++ tests/integration-test.js
@@ -4,22 +4,15 @@ var request = require('request')
 var expect = require('chai').expect
 var proxyURL = 'http://127.0.0.1:8000/api'
 var userId
-
+var user = { 'firstName': 'Mike 2', 'lastName': 'Doug', 'jobTitle': 'Tester', 'isFulltime': false }
 describe('Given we have REST API with users info', function () {
   describe('When POST new user', function () {
     it('it should return all users', function (done) {
       var options = {
         method: 'POST',
         url: proxyURL + '/users',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        form: {
-          'firstName': 'Mike 2',
-          'lastName': 'Doug',
-          'jobTitle': 'Tester',
-          'isFulltime': false
-        }
+        headers: { 'Content-Type': 'application/json' },
+        form: user
       }
       request(options, function (error, response, body) {
         if (error) {
@@ -49,8 +42,43 @@ describe('Given we have REST API with users info', function () {
       })
     })
   })
-  describe('When DELETE new user', function () {
+  describe('When PUT user with new data', function () {
     it('it should return all users', function (done) {
+      user.firstName = 'Mike 9'
+      var options = {
+        method: 'PUT',
+        url: proxyURL + '/users/' + userId,
+        headers: {},
+        form: user
+      }
+      request(options, function (error, response, body) {
+        if (error) {
+          throw Error('Error calling API' + error)
+        }
+        expect(response.statusCode).to.equal(200)
+        done()
+      })
+    })
+  })
+  describe('When PATCH user with new data', function () {
+    it('it should return all users', function (done) {
+      var options = {
+        method: 'PUT',
+        url: proxyURL + '/users/' + userId,
+        headers: {},
+        form: { 'lastName': 'Smith' }
+      }
+      request(options, function (error, response, body) {
+        if (error) {
+          throw Error('Error calling API' + error)
+        }
+        expect(response.statusCode).to.equal(200)
+        done()
+      })
+    })
+  })
+  describe('When DELETE new user', function () {
+    it('it should delete specific user', function (done) {
       var options = {
         method: 'DELETE',
         url: proxyURL + '/users/' + userId
@@ -60,7 +88,6 @@ describe('Given we have REST API with users info', function () {
           throw Error('Error calling API' + error)
         }
         expect(response.statusCode).to.equal(204)
-        userId = body._id
         done()
       })
     })
